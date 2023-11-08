cfcd5465151376a198bf5413fef8dca0e1244206
tahmid530 <tahmidahmed530@gmail.com>
2023-11-09 01:44:17 +0600

    data read successfully

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        const jobsCollection = client.db('jobsDB').collection('jobs');
+
+        // Data read
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
 
          // Data create
          app.post('/jobs', async (req, res) => {
